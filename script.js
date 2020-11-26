7a5e78fc12a9832c75c6cdc0c64beb87776342d7
Sofi Mirosans <sofiamirosans@gmail.com>
2020-11-26 21:08:39 +0100

    feedback updates

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-//1. Fetch global data
-//2. Display global data in global rows
-//3. Fetch country data
-//4. Select country
-//5. Display that specific country data in the country rows
-
 'use strict';
 
 //fetch global data 
@@ -21,7 +15,6 @@ fetch("https://api.covid19api.com/summary", requestOptions)
 //display global data
 
 function displayGlobalData(result) {
-console.log(result);
 $('#total-confirmed').html(result.Global.TotalConfirmed);
 $('#new-confirmed').html(result.Global.NewConfirmed);
 $('#total-deaths').html(result.Global.TotalDeaths);
@@ -30,38 +23,6 @@ $('#total-recovered').html(result.Global.TotalRecovered);
 $('#new-recovered').html(result.Global.NewRecovered);
 }
 
-/*function totalCasesSparkline (result) {
-  $("#sparkline").sparkline([result.Global.NewConfirmed], {
-    type: 'line',
-    lineColor: '#f87070',
-    fillColor: '#cdd7d6'});
-}*/
-
-/*function totalCasesSparkline (result) {
-  $("#sparkline").sparkline([5,6,7,9,9,5,3,2,2,4,6,7], {
-    type: 'line',
-    lineColor: '#f87070',
-    fillColor: '#cdd7d6'});
-   
-}*/
-
-//$("#sparkline").sparkline([5,6,7,9,9,5,3,2,2,4,6,7], {
-    //type: 'line',
-    //lineColor: '#f87070',
-    //fillColor: '#cdd7d6'});
-
-//fetch country data
-
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
-
-/*fetch("https://api.covid19api.com/countries", requestOptions)
-  .then(response => response.text())
-  .then(result => displayCountryData(result))
-  .catch(error => console.log('error', error));*/
-
 //select country 
 
 $(function selectCountry() {
@@ -69,6 +30,7 @@ $(function selectCountry() {
     .then(res => res.json())
     .then(res => {
       $("#countries").html("<option>Select...</option>");
+      res.sort(compareCountries);
       res.forEach(function (country) {
         $("#countries").append(`<option value="${country.Slug}">${country.Country}</option>`);
       });
@@ -78,12 +40,14 @@ $(function selectCountry() {
     });
 });
 
+//compare country strings to sort them alphabetically 
+
+function compareCountries(a,z) {
+if (a.Slug < z.Slug) return -1; 
+return 1; 
+}
+
 //obtain country data
-
-/*https://api.covid19api.com/total/country/south-africa
-status/confirmed*/
-
-//https://api.covid19api.com/total/country/south-africa
 
 function obtainCountryData(country) {
   fetch("https://api.covid19api.com/total/country/"+country, requestOptions)
@@ -107,3 +71,4 @@ $('#country-total-confirmed').html(country.Confirmed);
 $('#country-total-deaths').html(country.Deaths);
 $('#country-total-recovered').html(country.Recovered);
 }
+
