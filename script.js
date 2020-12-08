5b76cf3f99c1461ace04e29f31bc0ae723a246c8
Sofi Mirosans <sofiamirosans@gmail.com>
2020-12-08 15:14:52 +0100

    review adjustments

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ fetch("https://api.covid19api.com/summary", requestOptions)
 //display global data
 
 function displayGlobalData(result) {
-$('#total-confirmed').html(result.Global.TotalConfirmed);
-$('#new-confirmed').html(result.Global.NewConfirmed);
-$('#total-deaths').html(result.Global.TotalDeaths);
-$('#new-deaths').html(result.Global.NewDeaths);
-$('#total-recovered').html(result.Global.TotalRecovered);
-$('#new-recovered').html(result.Global.NewRecovered);
+  $('#total-confirmed').html(result.Global.TotalConfirmed);
+  $('#new-confirmed').html(result.Global.NewConfirmed);
+  $('#total-deaths').html(result.Global.TotalDeaths);
+  $('#new-deaths').html(result.Global.NewDeaths);
+  $('#total-recovered').html(result.Global.TotalRecovered);
+  $('#new-recovered').html(result.Global.NewRecovered);
 }
 
 //select country 
@@ -34,41 +34,41 @@ $(function selectCountry() {
       res.forEach(function (country) {
         $("#countries").append(`<option value="${country.Slug}">${country.Country}</option>`);
       });
-      $("#countries").on("change", function() {
-          obtainCountryData($(this).val());
-    });
+      $("#countries").on("change", function () {
+        obtainCountryData($(this).val());
+      });
     });
 });
 
 //compare country strings to sort them alphabetically 
 
-function compareCountries(a,z) {
-if (a.Slug < z.Slug) return -1; 
-return 1; 
+function compareCountries(a, z) {
+  if (a.Slug < z.Slug) return -1;
+  return 1;
 }
 
 //obtain country data
 
 function obtainCountryData(country) {
-  fetch("https://api.covid19api.com/total/country/"+country, requestOptions)
-  .then(response => response.json())
-  .then(result => displayCountryData(result[result.length-1]))
-  .catch(error => console.log('error', error));
+  fetch("https://api.covid19api.com/total/country/" + country, requestOptions)
+    .then(response => response.json())
+    .then(result => displayCountryData(result[result.length - 1]))
+    .catch(error => console.log('error', error));
 }
 
 //listen to country selected
 
 function watchSelect() {
- $("#countries").on("change", function obtainCountryData(countries) {
+  $("#countries").on("change", function obtainCountryData(countries) {
     displayCountryData(result)
-    });
+  });
 }
 
 //display country data
 
 function displayCountryData(country) {
-$('#country-total-confirmed').html(country.Confirmed);
-$('#country-total-deaths').html(country.Deaths);
-$('#country-total-recovered').html(country.Recovered);
+  $('#country-total-confirmed').html(country.Confirmed);
+  $('#country-total-deaths').html(country.Deaths);
+  $('#country-total-recovered').html(country.Recovered);
 }
 
